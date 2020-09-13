80b4391b9c4f182ce6572cc29ebf1ace1cb9f19c
hanseola <copetit412@gmail.com>
2020-09-13 16:32:36 +0900

    add key / alt

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React from "react";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h3>I like {name}</h3>
-      <img src={picture}></img>
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "kimchi",
     image:
       "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
   },
   {
+    id: 2,
     name: "kimbap",
     image:
       "https://www.thespruceeats.com/thmb/aPsmbaRgCtFLUTUFGXZBIAW93H8=/3797x2848/smart/filters:no_upscale()/kimbap-korean-sushi-rolls-2118795-Hero-5b7dbdd346e0fb00250718b8.jpg"
   },
   {
+    id: 3,
     name: "apple",
     image:
       "https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/apple-seasonal-colors-header-accessories-202006_FMT_WHH?wid=1472&hei=880&fmt=png-alpha&qlt=80&.v=1590688988000"
   }
 ];
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
