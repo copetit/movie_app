fe581fe5a6aafddd943d1da55455f0f83176a3a7
hanseola <copetit412@gmail.com>
2021-02-01 21:08:31 +0900

    modify navi design

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navi.css";
 
 function Navigation() {
   return (
-    <div>
+    <div className="navi">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
